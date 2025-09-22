4273bf9e73af38124099aa53898faad7b0b86bcd
daniel.graf@cybob.com <daniel.graf@cybob.com>
2025-09-22 13:11:30 +0000

    llm clean code

--- server/llm.php
+++ server/llm.php
@@ -27,6 +27,14 @@ $postFields = http_build_query([
     'conversation' => $conversation,
 ], '', '&', PHP_QUERY_RFC3986);
 
+$fields = [
+  'controller'   => 'message',
+  'message'      => $message,
+];
+if ($conversation !== '') $fields['conversation'] = $conversation;
+
+$postFields = http_build_query($fields);
+
 // cURL-POST
 $ch = curl_init($AGENT_URL);
 curl_setopt_array($ch, [
@@ -37,39 +45,19 @@ curl_setopt_array($ch, [
         'Content-Type: application/x-www-form-urlencoded',
         'Accept: application/json',
     ],
-    CURLOPT_FOLLOWLOCATION => true,
-    // CURLOPT_TIMEOUT        => 15,
     CURLOPT_ENCODING       => '', // gzip/deflate automatisch entpacken
 ]);
 $res  = curl_exec($ch);
-$err  = curl_error($ch);
 $code = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 if ($res === false) Response::fail(502, "Agent Request failed.");
 
 
-
-// Fehler?
-// if ($res === false) {
-    // http_response_code(502);
-    // echo json_encode(['error' => 'Upstream failed', 'detail' => $err, 'status' => $code], JSON_UNESCAPED_UNICODE);
-    // exit;
-// }
-// Status durchreichen & Antwort ausgeben
-// http_response_code($code ?: 200);
-// $elapsedMs = (int) round((microtime(true) - $startTime) * 1000);
-// $decoded = json_decode($res, true);
-//$decoded["reply"] = str_replace("KI","K-I-", $decoded["reply"]);
-//$decoded["reply"] = str_replace("twi-bot","twaybott", $decoded["reply"]);
-// echo json_encode(
-    // (json_last_error() === JSON_ERROR_NONE && is_array($decoded))
-        // ? $decoded + ['ms' => $elapsedMs]
-        // : ['ms' => $elapsedMs, 'raw' => $res],
-    // JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_SUBSTITUTE
-// );
+$decoded = json_decode($res, true);
 
 Response::success([
-    "reply" => "hi",
-    "ms" => $timer->getMs()
+    "ms" => $timer->getMs(),
+    "reply" => $decoded["reply"],
+    "conversation" => $decoded["conversation"],
 ]);
